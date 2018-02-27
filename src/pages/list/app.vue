f85ec0f0ebfee1b7781ff24e2387c5f988e4c694
wen911119 <wen911119@gmail.com>
2018-02-27 14:33:24 +0800

    update：list

--- src/pages/list/app.vue
+++ src/pages/list/app.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="wrapper">
-        <text>list</text>
+    <div>
+        <text>list4</text>
+        <slider class="slider" interval="3000" auto-play="true">
+            <div class="frame" v-for="img in imageList">
+                <image class="image" resize="cover" :src="img.src"></image>
+            </div>
+        </slider>
     </div>
 </template>
 
@@ -8,15 +13,35 @@
     export default {
         data() {
             return {
-                logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
+                imageList: [
+                    { src: 'https://gd2.alicdn.com/bao/uploaded/i2/T14H1LFwBcXXXXXXXX_!!0-item_pic.jpg' },
+                    { src: 'https://gd1.alicdn.com/bao/uploaded/i1/TB1PXJCJFXXXXciXFXXXXXXXXXX_!!0-item_pic.jpg' },
+                    { src: 'https://gd3.alicdn.com/bao/uploaded/i3/TB1x6hYLXXXXXazXVXXXXXXXXXX_!!0-item_pic.jpg' }
+                ]
             }
         }
     }
 </script>
 
 <style scoped>
-    .wrapper {
-        justify-content: center;
-        align-items: center;
+    .image {
+        width: 700px;
+        height: 700px;
+    }
+
+    .slider {
+        margin-top: 25px;
+        margin-left: 25px;
+        width: 700px;
+        height: 700px;
+        border-width: 2px;
+        border-style: solid;
+        border-color: #41B883;
+    }
+
+    .frame {
+        width: 700px;
+        height: 700px;
+        position: relative;
     }
 </style>
